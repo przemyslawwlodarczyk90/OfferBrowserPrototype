965892618d1537dbc643596f0097ea11c63afebc
Przemek <przemyslawwlodarczyk90@gmail.com>
2024-05-15 21:16:23 +0200

    Added empty facades

--- src/main/java/com/offerbrowserprototype/application/facades/LoginAndRegisterFacade.java
+++ src/main/java/com/offerbrowserprototype/application/facades/LoginAndRegisterFacade.java
@@ -1,4 +1,33 @@
 package com.offerbrowserprototype.application.facades;
 
+import com.offerbrowserprototype.application.services.LoginAndRegisterService;
+import com.offerbrowserprototype.domain.loginaandregister.dto.*;
+import com.offerbrowserprototype.domain.user.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LoginAndRegisterFacade {
+    private final LoginAndRegisterService service;
+
+    @Autowired
+    public LoginAndRegisterFacade(LoginAndRegisterService service) {
+        this.service = service;
+    }
+
+    public RegistrationResultDto register(RegisterUserDto userDto) {
+        return service.register(userDto);
+    }
+
+    public String login(LoginDto loginDto) {
+        return service.login(loginDto);
+    }
+
+    public UserDto updateUserProfile(UpdateUserDto updateUserDto) {
+        return service.updateUserProfile(updateUserDto);
+    }
+
+    public boolean changeUserPassword(ChangePasswordDto changePasswordDto) {
+        return service.changeUserPassword(changePasswordDto);
+    }
 }
--- src/main/java/com/offerbrowserprototype/application/facades/OfferFacade.java
+++ src/main/java/com/offerbrowserprototype/application/facades/OfferFacade.java
@@ -1,4 +1,38 @@
 package com.offerbrowserprototype.application.facades;
 
+import com.offerbrowserprototype.application.services.OfferService;
+import com.offerbrowserprototype.domain.offer.OfferDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class OfferFacade {
+    private final OfferService service;
+
+
+    public OfferFacade(OfferService service) {
+        this.service = service;
+    }
+
+    public OfferDto addOffer(OfferDto offerDto) {
+        return service.saveOffer(offerDto);
+    }
+
+    public OfferDto updateOffer(Long id, OfferDto offerDto) {
+        return service.updateOffer(id, offerDto);
+    }
+
+    public void deleteOffer(Long id) {
+        service.deleteOffer(id);
+    }
+
+    public OfferDto getOffer(Long id) {
+        return service.getOfferById(id);
+    }
+
+    public List<OfferDto> getAllOffers() {
+        return service.getAllOffers();
+    }
 }
