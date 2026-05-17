254181a0f9e03234cde1e39f8efb61af3ea7d0cf
Przemysław Włodarczyk <przemyslawwlodarczyk90@gmail.com>
2026-05-17 10:25:19 +0200

    fix(spa): fix invalid path pattern for Spring Boot 3 PathPatternParser

--- src/main/java/com/example/offerbrowserprototype/infrastructure/web/SpaController.java
+++ src/main/java/com/example/offerbrowserprototype/infrastructure/web/SpaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    @RequestMapping(value = {"/", "/{path:[^\\.]*}", "/{path:[^\\.]*}/**"})
     public String forward() {
         return "forward:/index.html";
     }
